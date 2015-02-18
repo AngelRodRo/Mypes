54b129b791bef16fd8857a113e3d64fd27a7959a
juliocsar13 <jc_redcsar@hotmail.com>
2015-02-18 01:47:25 -0500

    trabajando con responsive desing en los layouts

--- app/src/main/java/com/example/angel/mypes/MainActivity.java
+++ app/src/main/java/com/example/angel/mypes/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends SherlockActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item){
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
